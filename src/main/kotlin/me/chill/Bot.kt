979089774958a0ee3ef01cb8e521ddf6f1f91e2d
woojiahao <woojiahao1234@gmail.com>
2018-08-24 10:31:38 +0800

    Ignored config.json

--- src/main/kotlin/me/chill/Bot.kt
+++ src/main/kotlin/me/chill/Bot.kt
@@ -2,13 +2,17 @@ package me.chill
 
 import me.chill.configurations.loadConfigurations
 import me.chill.credentials.Credentials
+import net.dv8tion.jda.core.AccountType
+import net.dv8tion.jda.core.JDA
+import net.dv8tion.jda.core.JDABuilder
+import net.dv8tion.jda.core.OnlineStatus
 
 fun main(args: Array<String>) {
 	val configurations = loadConfigurations() ?: return
 	val credentials = Credentials(configurations)
-//	val jda: JDA = JDABuilder(AccountType.BOT)
-//		.setStatus(OnlineStatus.ONLINE)
-//		.setToken(credentials.token)
-//		.build()
+	val jda: JDA = JDABuilder(AccountType.BOT)
+		.setStatus(OnlineStatus.ONLINE)
+		.setToken(credentials.token)
+		.build()
 }
 
