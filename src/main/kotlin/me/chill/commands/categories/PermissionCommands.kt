b8e2139e8af106a77ea6764c41e36d45587f976c
woojiahao <woojiahao1234@gmail.com>
2018-08-25 18:47:50 +0800

    Completed the set permission command

--- src/main/kotlin/me/chill/commands/categories/PermissionCommands.kt
+++ src/main/kotlin/me/chill/commands/categories/PermissionCommands.kt
@@ -3,10 +3,13 @@ package me.chill.commands.categories
 import me.chill.commands.container.CommandContainer
 import me.chill.commands.container.command
 import me.chill.commands.container.commands
-import me.chill.database.viewPermissions
+import me.chill.database.*
+import me.chill.gifs.happy
+import me.chill.gifs.shock
 import me.chill.utility.green
 import me.chill.utility.jda.embed
 import me.chill.utility.jda.send
+import me.chill.utility.red
 import net.dv8tion.jda.core.entities.Guild
 
 fun permissionCommands() = commands {
@@ -17,17 +20,42 @@ fun permissionCommands() = commands {
 			val channel = getChannel()
 			val guild = getGuild()
 
+			val roles = guild.roles
+			val serverId = guild.id
+
 			val commandName = arguments[0]
 			val roleId = arguments[1]
 
+			if (!CommandContainer.hasCommand(commandName)) {
+				channel.send(setPermissionFailureEmbed("Command: **$commandName** does not exist"))
+				return@execute
+			}
 
+			if (guild.getRoleById(roleId) == null) {
+				channel.send(setPermissionFailureEmbed("Role: **$roleId** does not exist on **${guild.name}**"))
+				return@execute
+			}
+
+			val highestRole = roles[0].id
+			if (roleId == highestRole) {
+				removePermission(commandName, serverId)
+			} else {
+				if (hasPermission(commandName, serverId)) {
+					editPermission(commandName, serverId, roleId)
+				} else {
+					addPermission(commandName, serverId, roleId)
+				}
+			}
+			channel.send(
+				setPermissionSuccessEmbed("Command: **$commandName** has been assigned to **${guild.getRoleById(roleId).name}**")
+			)
 		}
 	}
 
 	command("viewpermissions") {
 		execute {
 			val guild = getGuild()
-			val channel = getChannel()	
+			val channel = getChannel()
 
 			val permissionMap = generatePermissionsMap(guild)
 			val permissionsList = generatePermissionsList(guild, permissionMap.toSortedMap())
@@ -35,6 +63,21 @@ fun permissionCommands() = commands {
 		}
 	}
 }
+
+private fun setPermissionStatusEmbed(status: String, color: Int,
+									 thumbnail: String, message: String) =
+	embed {
+		title = "Set Permission $status!"
+		this.color = color
+		description = message
+		this.thumbnail = thumbnail
+	}
+
+private fun setPermissionSuccessEmbed(message: String) =
+	setPermissionStatusEmbed("Success", green, happy, message)
+
+private fun setPermissionFailureEmbed(message: String) =
+	setPermissionStatusEmbed("Failed", red, shock, message)
 
 private fun generatePermissionsMap(guild: Guild): MutableMap<String, String> {
 	val highestRole = guild.roles[0].id
--- src/main/kotlin/me/chill/database/PermissionOperations.kt
+++ src/main/kotlin/me/chill/database/PermissionOperations.kt
@@ -1,6 +1,7 @@
 package me.chill.database
 
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun addPermission(commandName: String, serverId: String, roleId: String) {
@@ -15,19 +16,23 @@ fun addPermission(commandName: String, serverId: String, roleId: String) {
 
 fun removePermission(commandName: String, serverId: String) {
 	transaction {
-		Permission.deleteWhere {
-			(Permission.serverId eq serverId) and (Permission.commandName eq commandName)
-		}
+		Permission.deleteWhere { selectKey(serverId, commandName) }
 	}
 }
 
 fun editPermission(commandName: String, serverId: String, roleId: String) {
 	transaction {
-		Permission.update({ (Permission.serverId eq serverId) and (Permission.commandName eq commandName) }) {
+		Permission.update({ selectKey(serverId, commandName) }) {
 			it[Permission.permission] = roleId
 		}
 	}
 }
+
+fun hasPermission(commandName: String, serverId: String) =
+	transaction {
+		Permission.select { selectKey(serverId, commandName) }.any()
+	}
+
 
 fun viewPermissions(serverId: String): MutableMap<String, String> {
 	val commandMap = mutableMapOf<String, String>()
@@ -40,3 +45,6 @@ fun viewPermissions(serverId: String): MutableMap<String, String> {
 	}
 	return commandMap
 }
+
+private fun selectKey(serverId: String, commandName: String) =
+	(Permission.serverId eq serverId) and (Permission.commandName eq commandName)
