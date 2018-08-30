91033acda5dec48841e12497690eeda763fe518b
woojiahao <woojiahao1234@gmail.com>
2018-08-30 10:51:09 +0800

    Used regex to block everyone/here/role/member mentions via echo

--- src/main/kotlin/me/chill/commands/type/ModerationCommands.kt
+++ src/main/kotlin/me/chill/commands/type/ModerationCommands.kt
@@ -3,7 +3,9 @@ package me.chill.commands.type
 import me.chill.commands.arguments.ArgumentType.*
 import me.chill.commands.framework.CommandCategory
 import me.chill.commands.framework.commands
+import me.chill.utility.jda.failureEmbed
 import me.chill.utility.jda.send
+import me.chill.utility.settings.noWay
 import net.dv8tion.jda.core.entities.MessageHistory
 
 @CommandCategory
@@ -29,7 +31,18 @@ fun moderationCommands() = commands {
 		expects(ChannelId, Sentence)
 		execute {
 			val args = getArguments()
+			val message = args[1] as String
 			val messageChannel = getGuild().getTextChannelById(args[0] as String)
+			if (message.contains(Regex("(<@\\d*>)|(<@&\\d*>)|(<@everyone>)|(<@here>)"))) {
+				respond(
+					failureEmbed(
+						"Echo",
+						"Cannot echo a message with a member/role mention",
+						thumbnail = noWay
+					)
+				)
+				return@execute
+			}
 			messageChannel.send(args[1] as String)
 		}
 	}
