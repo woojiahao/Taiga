735b9f861ef042dfce342d154785f0d5f700db32
woojiahao <woojiahao1234@gmail.com>
2018-09-06 08:10:47 +0800

    Allowed for lowercase command names and category names

--- src/main/kotlin/me/chill/arguments/types/CategoryName.kt
+++ src/main/kotlin/me/chill/arguments/types/CategoryName.kt
@@ -4,9 +4,10 @@ import me.chill.arguments.Argument
 import me.chill.arguments.ParseMap
 import me.chill.framework.CommandContainer
 import net.dv8tion.jda.core.entities.Guild
+import org.apache.commons.lang3.text.WordUtils
 
 class CategoryName : Argument {
 	override fun check(guild: Guild, arg: String) =
-		if (!CommandContainer.hasCategory(arg)) ParseMap(false, "Category: **$arg** does not exist")
-		else ParseMap(parseValue = arg)
+		if (!CommandContainer.hasCategory(arg.toLowerCase())) ParseMap(false, "Category: **$arg** does not exist")
+		else ParseMap(parseValue = WordUtils.capitalize(arg))
 }
--- src/main/kotlin/me/chill/arguments/types/CommandName.kt
+++ src/main/kotlin/me/chill/arguments/types/CommandName.kt
@@ -4,9 +4,10 @@ import me.chill.arguments.Argument
 import me.chill.arguments.ParseMap
 import me.chill.framework.CommandContainer
 import net.dv8tion.jda.core.entities.Guild
+import org.apache.commons.lang3.text.WordUtils
 
 class CommandName : Argument {
 	override fun check(guild: Guild, arg: String) =
-		if (!CommandContainer.hasCommand(arg)) ParseMap(false, "Command: **$arg** does not exist")
-		else ParseMap(parseValue = arg)
+		if (!CommandContainer.hasCommand(arg.toLowerCase())) ParseMap(false, "Command: **$arg** does not exist")
+		else ParseMap(parseValue = arg.toLowerCase())
 }
