5f73bbcc4db5f99b92700721dec9cdc240a63165
woojiahao <woojiahao1234@gmail.com>
2018-09-18 11:43:14 +0800

    Strictened the Prefix argument type such that the user cannot use all letters/digits in their prefix

--- src/main/kotlin/me/chill/arguments/types/Prefix.kt
+++ src/main/kotlin/me/chill/arguments/types/Prefix.kt
@@ -6,6 +6,9 @@ import net.dv8tion.jda.core.entities.Guild
 
 class Prefix : Argument {
 	override fun check(guild: Guild, arg: String) =
-		if (arg.length > 3) ArgumentParseMap(false, "Prefix should be less than 3 characters")
-		else ArgumentParseMap(true, parsedValue = arg)
+		when {
+			arg.length > 3 -> ArgumentParseMap(false, "Prefix should be less than 3 characters")
+			arg.all { it.isLetterOrDigit() } -> ArgumentParseMap(false, "Prefix cannot be all letters or digits")
+			else -> ArgumentParseMap(true, parsedValue = arg)
+		}
 }
