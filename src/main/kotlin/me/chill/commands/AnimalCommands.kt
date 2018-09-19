2fc2879643433832cef6b939978a44ae8566b238
woojiahao <woojiahao1234@gmail.com>
2018-09-19 13:30:08 +0800

    Made all animal commands available to anyone on join

--- src/main/kotlin/me/chill/commands/AnimalCommands.kt
+++ src/main/kotlin/me/chill/commands/AnimalCommands.kt
@@ -11,6 +11,7 @@ import java.net.URL
 
 @CommandCategory
 fun animalCommands() = commands("Animal") {
+	setGlobal()
 	command("cat") {
 		execute {
 			val result = Gson().fromJson(URL("https://aws.random.cat/meow").readText(), JsonObject::class.java)
