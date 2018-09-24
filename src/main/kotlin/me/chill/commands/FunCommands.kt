b2ce154a785a7313212b6f6e00f325cffffd54d4
woojiahao <woojiahao1234@gmail.com>
2018-09-24 08:50:00 +0800

    Added cookie command

--- src/main/kotlin/me/chill/commands/FunCommands.kt
+++ src/main/kotlin/me/chill/commands/FunCommands.kt
@@ -1,6 +1,7 @@
 package me.chill.commands
 
 import me.chill.arguments.types.Sentence
+import me.chill.arguments.types.UserId
 import me.chill.framework.CommandCategory
 import me.chill.framework.commands
 import me.chill.utility.str
@@ -8,6 +9,7 @@ import java.util.*
 
 @CommandCategory
 fun funCommands() = commands("Fun") {
+	setGlobal()
 	command("clapify") {
 		expects(Sentence())
 		execute {
@@ -23,4 +25,22 @@ fun funCommands() = commands("Fun") {
 			)
 		}
 	}
+
+	command("cookie") {
+		execute {
+			val memberList = guild.members
+			val randomUser = memberList[Random().nextInt(memberList.size + 1)]
+			respond("(づ｡◕‿‿◕｡)づ Have a cookie :cookie: ${randomUser.asMention} courtesy of ${invoker.asMention}")
+		}
+	}
+
+	command("cookie") {
+		expects(UserId())
+		execute {
+			respond(cookieGiving(guild.getMemberById(arguments[0]!!.str()).asMention, invoker.asMention))
+		}
+	}
 }
+
+private fun cookieGiving(target: String, from: String) =
+	"(づ｡◕‿‿◕｡)づ Have a cookie :cookie: $target courtesy of $from"
