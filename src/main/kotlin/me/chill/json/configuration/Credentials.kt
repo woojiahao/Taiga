4172a671270d1801fc8bd3a30ba4fcea9597e768
woojiahao <woojiahao1234@gmail.com>
2019-04-11 09:59:43 +0800

    Credentials as global var
    Credentials are now read directly from environment variables

--- src/main/kotlin/me/chill/json/configuration/Credentials.kt
+++ src/main/kotlin/me/chill/json/configuration/Credentials.kt
@@ -2,6 +2,43 @@ package me.chill.json.configuration
 
 import me.chill.exception.CredentialException
 
+fun env(name: String, default: String = "", err: () -> Unit = { }): String {
+  val env = System.getenv(name)
+  return if (env == null) {
+    err()
+    default
+  } else env
+}
+
+val token = env("BOT_TOKEN") {
+  throw CredentialException(
+    "Token",
+    "Set token environment variable for Heroku to proceed"
+  )
+}
+
+val databaseUrl = env("JDBC_DATABASE_URL") {
+  throw CredentialException(
+    "Database",
+    "Set a default prefix environment variable in Heroku to proceed"
+  )
+}
+
+val defaultPrefix = env("PREFIX") {
+  throw CredentialException(
+    "Default Prefix",
+    "Set a default prefix environment variable in Heroku to proceed"
+  )
+}
+
+val lyricsApiKey = env("LYRICS_API") {
+  println("NOTE :: Song commands will not work without the Lyrics API key")
+}
+
+val songNameApiKey = env("SONG_NAME_API") {
+  println("NOTE :: Song commands will not work without the Song Name API key")
+}
+
 class Credentials(configuration: Configuration?) {
   var token: String? = ""
   var database: String? = ""
